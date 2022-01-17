2ef4c1477e179e3320a9a414bd0b9a7d2665e5d5
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-17 16:17:17 -0300

    Rotas importadas.

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 
+const MONGO_DB_URL = `mongodb://${process.env.HOST || 'mongodb'}:27017/Cookmaster`;
+const DB_NAME = 'Cookmaster';
+const {
+  usersRouter,
+  loginRouter,
+  recipesRouter,
+  imagesRouter,
+} = require('./routes');
+
 const app = express();
+
+app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/recipes', recipesRouter);
+app.use('/images', imagesRouter);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
