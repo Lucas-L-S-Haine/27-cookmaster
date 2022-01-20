5a2a3cc844773d44fd5a71f397a9942fe2b3add9
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-20 20:19:28 -0300

    Desestrutura funções de models. Acrescenta o role no Joi. Tratamento de erros.

--- src/api/services/usersServices.js
+++ src/api/services/usersServices.js
@@ -1,10 +1,11 @@
 const Joi = require('joi');
-const usersModel = require('../models/usersModel');
+const { insertUser, findUserByEmail } = require('../models/usersModel');
 
 const userSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+    role: Joi.string().required(),
 });
 
 const newError = (err) => (err);
@@ -12,9 +13,13 @@ const newError = (err) => (err);
 const newUserValidate = async (user) => {
   const { error } = userSchema.validate(user);
   if (error) {
-    throw newError({ status: 333, message: error.message });
+    console.log(error);
+    throw newError({ status: 400, message: 'Invalid entries. Try again.' });
   }
-  const createdUser = await usersModel.insertUser(user);
+  if (!(await findUserByEmail(user.email))) {
+    throw newError({ status: 355, message: error.message });
+  }
+  const createdUser = await insertUser(user);
   return createdUser;
 };
 module.exports = {
