e791c9ef1682602d327b08a17a61f60c506992ca
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-21 15:44:44 -0300

    Requisito 02 concluído.

--- src/api/controllers/loginController.js
+++ src/api/controllers/loginController.js
@@ -12,8 +12,8 @@ const insert = async (req, res, _next) => {
   try {
     const login = req.body;
     const response = await loginValidate(login);
-    const token = jwt.sign({ login }, secret, jwtConfig);
-    console.log(token);
+    const payload = response.ops[0];
+    const token = jwt.sign({ payload }, secret, jwtConfig);
     return res.status(200).json(token);
   } catch (err) {
     return res.status(err.status).json({ message: err.message });
