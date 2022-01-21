de19e0c07a77a1e31c21a5bf0ac9c9686f514a8a
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-21 01:20:06 -0300

    Retornando "um" token. Ainda faltam as informações corretas no payload.

--- src/api/controllers/loginController.js
+++ src/api/controllers/loginController.js
@@ -1,10 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'secret';
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
 const { loginValidate } = require('../services/loginServices');
 
 const insert = async (req, res, _next) => {
   try {
     const login = req.body;
     const response = await loginValidate(login);
-    return res.status(200).json(response.ops[0]);
+    const token = jwt.sign({ login }, secret, jwtConfig);
+    console.log(token);
+    return res.status(200).json(token);
   } catch (err) {
     return res.status(err.status).json({ message: err.message });
   }
