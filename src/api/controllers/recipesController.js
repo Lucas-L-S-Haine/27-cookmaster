38fcc849b06c5d54765d5ef602fba238ba94b0bb
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-25 18:25:22 -0300

    Métodos de update e delete implementados.

--- src/api/controllers/recipesController.js
+++ src/api/controllers/recipesController.js
@@ -1,18 +1,22 @@
 const {
   readAllRecipes,
+  updateRecipe,
 } = require('../models/recipesModel');
 
 const {
+  readRecipeValidate,
+  newRecipeValidate,
+  ownerValidate,
+  deletedRecipeValidate,
 } = require('../services/recipesServices');
 
 const insert = async (req, res) => {
-  try {
-    const recipeData = req.body;
-    const response = await recipeValidate(recipeData);
-    return res.status(201).json(response);
-  } catch (err) {
-    return res.status(err.status).json({ message: err.message });
-  }
+  const { name, ingredients, preparation } = req.body;
+  const { _id: userId } = req.user;
+  const recipe = await newRecipeValidate(
+    name, ingredients, preparation, userId,
+  );
+  return res.status(201).json(recipe);
 };
 
 const readAll = async (req, res) => {
@@ -26,9 +30,22 @@ const read = async (req, res) => {
   return res.status(200).json(response);
 };
 
-const update = () => {};
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { name, ingredients, preparation } = req.body;
+  const { _id: userId, role } = req.user;
+  ownerValidate(id, userId, role);
+  const recipe = await updateRecipe(id, name, ingredients, preparation);
+  return res.status(200).json(recipe);
+};
 
-const remove = () => {};
+const remove = async (req, res) => {
+  const { id } = req.params;
+  const { _id: userId, role } = req.user;
+  await ownerValidate(id, userId, role);
+  await deletedRecipeValidate(id);
+  return res.status(204).end();
+};
 
 const updateImage = () => {};
 
--- src/api/services/recipesServices.js
+++ src/api/services/recipesServices.js
@@ -1,5 +1,7 @@
 const {
-  insertRecipe, readRecipe, readAllRecipes,
+  insertRecipe,
+  readRecipe,
+  deleteRecipe,
 } = require('../models/recipesModel');
 
 const {
@@ -8,3 +10,35 @@ const {
   validRecipe,
 } = require('../utils/functions');
 
+const readRecipeValidate = async (id) => {
+  const result = await readRecipe(id);
+  validRecipe(result);
+  return result;
+};
+
+const newRecipeValidate = async (name, ingredients, preparation, userId) => {
+  recipeValidate(name, ingredients, preparation);
+  const recipe = await insertRecipe(name, ingredients, preparation, userId);
+  return recipe;
+};
+
+const deletedRecipeValidate = async (id) => {
+  const recipe = await deleteRecipe(id);
+  validRecipe(recipe);
+  return recipe;
+};
+
+const ownerValidate = async (recipeId, userId, userRole) => {
+  const recipe = await readRecipe(recipeId);
+  validRecipe(recipe);
+  if (recipe.userId !== userId && userRole !== 'admin') {
+    throw newError({ status: 401, message: 'unauthorized' });
+  }
+};
+
+module.exports = {
+  readRecipeValidate,
+  newRecipeValidate,
+  deletedRecipeValidate,
+  ownerValidate,
+};
