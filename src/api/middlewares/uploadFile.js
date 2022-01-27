66da4d046530cff6060e53bd1c37b70ec8c994a0
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-01-27 03:33:29 -0300

    Mudança no caminho do diretório de uploads.

--- src/api/middlewares/uploadFile.js
+++ src/api/middlewares/uploadFile.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, join(__dirname, '..', 'uploads'));
+    cb(null, join(__dirname, '../..', 'uploads'));
   },
   filename: (req, file, cb) => {
     const { id } = req.params;
